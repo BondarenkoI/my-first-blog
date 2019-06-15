fb47cd1b486d24375c9c038f7709babe1e0ba52b
BondarenkoI <kapitankamsuno@gmail.com>
2019-06-15 21:05:03 +0300

    My dj Girls first commit

---
+++ djangovenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/vladimir/ilya/projects/django/djangogirls/djangovenv/bin/python3.5
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
